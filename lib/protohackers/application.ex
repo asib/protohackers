c98fcfae5b91fb5568965c9cc22f58b1c04e838a
Jacob Fenton <jacob+github@dfenton.xyz>
2023-05-28 15:18:59 +0300

    Better error messages for incorrect arguments.

--- lib/protohackers/application.ex
+++ lib/protohackers/application.ex
@@ -7,21 +7,33 @@ defmodule Protohackers.Application do
 
   @impl true
   def start(_type, _args) do
-
-    with port_string when not is_nil(port_string) <- System.get_env("PORT"),
+    with {:ok, port_string} <- fetch_env("PORT"),
          port <- String.to_integer(port_string),
-         app_name when not is_nil(app_name) <- System.get_env("APP_NAME") do
+         {:ok, app_name} <- fetch_env("APP_NAME"),
+         client_handler_name <- Module.concat([Protohackers, Macro.camelize(app_name), Client]),
+         {:module, client_handler} <- ensure_compiled(client_handler_name) do
 
-        client_handler = Module.concat([Protohackers, Macro.camelize(app_name), Client])
         children = [
           {Protohackers.Server, [port: port, client_handler: client_handler]}
         ]
 
         opts = [strategy: :one_for_one, name: Protohackers.Supervisor]
         Supervisor.start_link(children, opts)
-      else
-        _ -> {:error, "Must specify PORT and APP_NAME"}
     end
 
+  end
+
+  defp fetch_env(var_name) do
+    case System.fetch_env(var_name) do
+      :error -> {:error, "#{var_name} not found"}
+      ok -> ok
+    end
+  end
+
+  defp ensure_compiled(module_name) do
+    case Code.ensure_compiled(module_name) do
+      {:error, err} -> {:error, "could not load client handler module #{module_name}: #{err}"}
+      ok -> ok
+    end
   end
 end

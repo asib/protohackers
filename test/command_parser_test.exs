7d2f081405b67d46a65c80e004185f11eca0049e
Jacob Fenton <jacob+github@dfenton.xyz>
2023-05-30 11:04:16 +0300

    Additional incomplete tests.

--- test/command_parser_test.exs
+++ test/command_parser_test.exs
@@ -4,19 +4,19 @@ defmodule CommandParserTest do
   import Protohackers.VoraciousCodeStorage.CommandParser
   alias Protohackers.VoraciousCodeStorage.CommandParser
 
-  @help_cases [
+  @help_success_cases [
     {"help", "can parse help"},
     {"help bla bla", "parsing help ignores any input after the command but before the newline"}
   ]
 
-  for {input, case_name} <- @help_cases do
+  for {input, case_name} <- @help_success_cases do
     test case_name do
       assert parse("#{unquote(input)}\n") == {:ok, :help, ""}
     end
   end
 
   @commands_with_path [{:list, CommandParser.List}, {:get, CommandParser.Get}]
-  @command_with_path_cases [
+  @command_with_path_success_cases [
     {"/", "/", "can parse root"},
     {"/bla.txt", "/bla.txt", "can parse file in root"},
     {"/bla-testing_new.txt", "/bla-testing_new.txt", "can parse file with hyphen"},
@@ -25,21 +25,21 @@ defmodule CommandParserTest do
   ]
 
   for {command, struct_name} <- @commands_with_path,
-      {input, expected, case_name} <- @command_with_path_cases do
+      {input, expected, case_name} <- @command_with_path_success_cases do
     test "#{Atom.to_string(command)}: #{case_name}" do
       assert parse("#{Atom.to_string(unquote(command))} #{unquote(input)}\n") ==
                {:ok, %unquote(struct_name){path: unquote(expected)}, ""}
     end
   end
 
-  @put_cases [
+  @put_success_cases [
     {"/bla.txt 5\nhello", {"/bla.txt", "hello"}, "can parse put"},
     {"/test 2b\na\n", {"/test", "a\n"}, "ignore characters after length but before newline"},
     {"/test bla\n", {"/test", ""}, "(1): non-numeric length is parsed as 0"},
     {"/test _\n", {"/test", ""}, "(2): non-numeric length is parsed as 0"}
   ]
 
-  for {input, {path, data}, case_name} <- @put_cases do
+  for {input, {path, data}, case_name} <- @put_success_cases do
     test case_name do
       assert parse("put #{unquote(input)}") ==
                {:ok, %CommandParser.Put{path: unquote(path), data: unquote(data)}, ""}
@@ -51,7 +51,11 @@ defmodule CommandParserTest do
   end
 
   @incomplete_cases [
-    "help"
+    "help",
+    "get /bla.txt",
+    "list ",
+    "put /bla.txt 15",
+    "put /bla.txt ddfkdfd"
   ]
 
   for input <- @incomplete_cases do

a83d7ce966d7e49b1b15afa3b9b240ed7515d89e
Jacob Fenton <jacob+github@dfenton.xyz>
2023-06-01 00:45:39 +0300

    Use active: once mode to simplify reading put data.

--- lib/protohackers/voracious_code_storage/client.ex
+++ lib/protohackers/voracious_code_storage/client.ex
@@ -9,7 +9,7 @@ defmodule Protohackers.VoraciousCodeStorage.Client do
   defstruct [:socket, :buffer]
 
   def server_socket_opts do
-    [packet: :line]
+    [packet: :line, active: :once]
   end
 
   def applications do
@@ -25,44 +25,52 @@ defmodule Protohackers.VoraciousCodeStorage.Client do
   end
 
   def handle_continue(:ready, state) do
+    Logger.info("READY\n")
     sendmsg(state, "READY")
 
     {:noreply, state}
   end
 
   def handle_info({:tcp, _socket, data}, %{buffer: buffer} = state) do
+    Logger.info("#{inspect(state.socket)}: #{inspect(data)}")
+
     case CommandParser.parse(data) do
       {:error, :no_newline} ->
-        Logger.error("got incomplete message")
+        Logger.error("#{inspect(state.socket)}: got incomplete message")
         noreply(%{state | buffer: buffer <> data})
 
       {:error, {:illegal_method, method}} ->
-        Logger.info("illegal method: #{method}")
+        Logger.info("#{inspect(state.socket)}: illegal method: #{method}")
         senderr(state, "illegal method: #{method}")
         {:stop, :normal, state}
 
       {:error, {:usage, command}} ->
-        Logger.info("usage: #{usage(command)}\n")
+        Logger.info("#{inspect(state.socket)}: usage: #{usage(command)}\n")
         senderr(state, "usage: #{usage(command)}")
         noreply(state)
 
       {:error, :illegal_dir_name} ->
+        Logger.info("#{inspect(state.socket)}: illegal dir name\n")
         senderr(state, "illegal dir name")
         noreply(state)
 
       {:error, :illegal_file_name} ->
+        Logger.info("#{inspect(state.socket)}: illegal file name\n")
         senderr(state, "illegal file name")
         noreply(state)
 
       {:error, :invalid_revision} ->
+        Logger.info("#{inspect(state.socket)}: no such revision\n")
         senderr(state, "no such revision")
         ready(state)
 
       {:ok, :help} ->
+        Logger.info("#{inspect(state.socket)}: HELP\n")
         sendmsg(state, "OK usage: HELP|GET|PUT|LIST")
         ready(state)
 
       {:ok, %CommandParser.List{path: path}} ->
+        Logger.info("#{inspect(state.socket)}: #{data}")
         files_in_path = FileSystem.list(path)
         sendmsg(state, "OK #{Enum.count(files_in_path)}")
 
@@ -73,6 +81,8 @@ defmodule Protohackers.VoraciousCodeStorage.Client do
         ready(state)
 
       {:ok, %CommandParser.Get{path: path, revision: revision}} ->
+        Logger.info("#{inspect(state.socket)}: #{data}")
+
         case FileSystem.get(path, revision) do
           {:error, :no_such_file} ->
             senderr(state, "no such file")
@@ -86,7 +96,8 @@ defmodule Protohackers.VoraciousCodeStorage.Client do
         ready(state)
 
       {:ok, %CommandParser.Put{path: path, length: length}} ->
-        :inet.setopts(state.socket, active: false, packet: :raw)
+        Logger.info("#{inspect(state.socket)}: #{data}")
+        :inet.setopts(state.socket, packet: :raw)
 
         case :gen_tcp.recv(state.socket, length) do
           {:ok, file_data} ->
@@ -99,7 +110,9 @@ defmodule Protohackers.VoraciousCodeStorage.Client do
             Logger.error(inspect(err))
         end
 
-        :inet.setopts(state.socket, active: true, packet: :line)
+        Logger.info("#{inspect(state.socket)}: read file data")
+
+        :inet.setopts(state.socket, packet: :line)
         ready(state)
     end
   end
@@ -117,12 +130,12 @@ defmodule Protohackers.VoraciousCodeStorage.Client do
   end
 
   defp ready(state) do
-    # :inet.setopts(state.socket, active: :once)
+    :inet.setopts(state.socket, active: :once)
     {:noreply, state, {:continue, :ready}}
   end
 
   defp noreply(state) do
-    # :inet.setopts(state.socket, active: :once)
+    :inet.setopts(state.socket, active: :once)
     {:noreply, state}
   end
 
